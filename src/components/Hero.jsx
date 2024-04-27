7bcc0a5ee612e525cbb30a2be67f1b6e8e286563
itzrizvi <Shahriar.me.work@gmail.com>
2024-04-27 12:09:21 +0600

    Hide hero animations on mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,16 @@
 import { motion } from "framer-motion";
+
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
-import { useCallback } from "react";
+import { useWindowSize } from "../utils/useWindowSize";
 
 const Hero = () => {
-  const computerCanvas = useCallback(() => {
-    return <ComputersCanvas />;
-  }, []);
+  const { width } = useWindowSize();
+  const isMobile = width < 640;
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+    <section
+      className={`relative w-full ${isMobile ? "h-80" : "h-screen"} mx-auto`}
+    >
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
@@ -30,26 +32,27 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* <ComputersCanvas /> */}
-      {computerCanvas()}
+      {!isMobile && <ComputersCanvas />}
 
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-        <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.div
-              animate={{
-                y: [0, 24, 0]
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop"
-              }}
-              className="w-3 h-3 rounded-full bg-secondary mb-1"
-            />
-          </div>
-        </a>
-      </div>
+      {!isMobile && (
+        <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+          <a href="#about">
+            <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+              <motion.div
+                animate={{
+                  y: [0, 24, 0]
+                }}
+                transition={{
+                  duration: 1.5,
+                  repeat: Infinity,
+                  repeatType: "loop"
+                }}
+                className="w-3 h-3 rounded-full bg-secondary mb-1"
+              />
+            </div>
+          </a>
+        </div>
+      )}
     </section>
   );
 };
