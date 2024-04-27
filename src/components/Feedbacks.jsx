0107834c6ca73fa7922d2e35ffe3af11c5da02b8
itzrizvi <Shahriar.me.work@gmail.com>
2024-04-27 13:14:39 +0600

    Feedback alignment fixed

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -16,7 +16,7 @@ const FeedbackCard = ({
 }) => (
   <motion.div
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
-    className="bg-black-200 p-10 rounded-3xl w-full md:w-[250px] sm:w-[250px] xs:w-full"
+    className="bg-black-200 p-10 rounded-3xl w-full md:w-[320px] sm:w-[320px] xs:w-full"
   >
     <p className="text-white font-black text-[48px]">"</p>
 
